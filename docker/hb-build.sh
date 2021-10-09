e3ebe3fb5a55352a5115d55810da54179940acbf
李昊甦 <lihs@botech.com.cn>
2021-10-09 14:33:29 +0800

    修改docker打包配置，增加翻墙代理支持

---
+++ docker/hb-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+export HB_PROXY_ON=1
+export HB_PROXY_HTTP_IP=192.168.202.59
+export HB_PROXY_HTTP_PORT=7890
+export DATAHUB_VERSION=my
+
+if [ "$HB_PROXY_ON" == "1" ];then
+  echo "开启HTTP代理【${HB_PROXY_HTTP_IP}:${HB_PROXY_HTTP_PORT}】"
+  export HTTP_PROXY=http://${HB_PROXY_HTTP_IP}:${HB_PROXY_HTTP_PORT}
+  export HTTPS_PROXY=http://${HB_PROXY_HTTP_IP}:${HB_PROXY_HTTP_PORT}
+  
+  COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -p datahub build --build-arg HB_PROXY_CURL_OPT="-x ${HB_PROXY_HTTP_IP}:${HB_PROXY_HTTP_PORT}" --build-arg HB_PROXY_WGET_OPT="-e http_proxy=${HB_PROXY_HTTP_IP}:${HB_PROXY_HTTP_PORT} -e https_proxy=${HB_PROXY_HTTP_IP}:${HB_PROXY_HTTP_PORT} -e use_proxy=on" --build-arg HB_PROXY_GRADLE_OPT="-Dhttp.proxyHost=${HB_PROXY_HTTP_IP} -Dhttp.proxyPort=${HB_PROXY_HTTP_PORT} -Dhttps.proxyHost=${HB_PROXY_HTTP_IP} -Dhttps.proxyPort=${HB_PROXY_HTTP_PORT}"
+else
+  COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -p datahub build
+fi
+
+./hb-start.sh
