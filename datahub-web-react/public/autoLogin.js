40aeccbb95be8d44f2036833ca2facf0acba578e
zhanglong <zhanglong6740@163.com>
2021-12-31 13:52:48 +0800

    处理自动登录BUG

--- datahub-web-react/public/autoLogin.js
+++
@@ -1,8 +0,0 @@
-const requestOptions = {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ username: 'datahub', password: 'datahub' }),
-};
-fetch('/logIn', requestOptions).then(()=>{
-
-})
