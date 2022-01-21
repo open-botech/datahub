3bc5672e7cfe4a2aca14a298e51786a0cee7792e
zhanglong <zhanglong6740@163.com>
2022-01-21 13:38:57 +0800

    首页模块更改

--- datahub-web-react/src/app/home/HomePageRecommendations.tsx
+++ datahub-web-react/src/app/home/HomePageRecommendations.tsx
@@ -1,15 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { Button, Divider, Empty, Typography } from 'antd';
-import { RocketOutlined } from '@ant-design/icons';
+import { Divider, Typography,Row, Col} from 'antd';
 import { RecommendationModule as RecommendationModuleType, ScenarioType } from '../../types.generated';
 import { useListRecommendationsQuery } from '../../graphql/recommendations.generated';
 import { RecommendationModule } from '../recommendations/RecommendationModule';
 import { BrowseEntityCard } from '../search/BrowseEntityCard';
 import { useEntityRegistry } from '../useEntityRegistry';
 import { useGetEntityCountsQuery } from '../../graphql/app.generated';
-import { GettingStartedModal } from './GettingStartedModal';
-import { ANTD_GRAY } from '../entity/shared/constants';
 
 const RecommendationsContainer = styled.div`
     margin-top: 32px;
@@ -19,8 +16,6 @@ const RecommendationsContainer = styled.div`
 
 const RecommendationContainer = styled.div`
     margin-bottom: 32px;
-    max-width: 1000px;
-    min-width: 750px;
 `;
 
 const RecommendationTitle = styled(Typography.Title)`
@@ -41,22 +36,6 @@ const BrowseCardContainer = styled.div`
     flex-wrap: wrap;
 `;
 
-const ConnectSourcesButton = styled(Button)`
-    margin: 16px;
-`;
-
-const NoMetadataEmpty = styled(Empty)`
-    font-size: 18px;
-    color: ${ANTD_GRAY[8]};
-`;
-
-const NoMetadataContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`;
-
 type Props = {
     userUrn: string;
 };
@@ -65,7 +44,6 @@ export const HomePageRecommendations = ({ userUrn }: Props) => {
     // Entity Types
     const entityRegistry = useEntityRegistry();
     const browseEntityList = entityRegistry.getBrowseEntityTypes();
-    const [showGettingStartedModal, setShowGettingStartedModal] = useState(false);
 
     const { data: entityCountData } = useGetEntityCountsQuery({
         variables: {
@@ -88,36 +66,28 @@ export const HomePageRecommendations = ({ userUrn }: Props) => {
                 requestContext: {
                     scenario,
                 },
-                limit: 10,
+                limit: 5,
             },
         },
         fetchPolicy: 'no-cache',
     });
     const recommendationModules = data?.listRecommendations?.modules;
-
-    // Determine whether metadata has been ingested yet.
-    const hasLoadedEntityCounts = orderedEntityCounts && orderedEntityCounts.length > 0;
-    const hasIngestedMetadata =
-        orderedEntityCounts && orderedEntityCounts.filter((entityCount) => entityCount.count > 0).length > 0;
-
-    useEffect(() => {
-        if (hasLoadedEntityCounts && !hasIngestedMetadata) {
-            setShowGettingStartedModal(true);
-        }
-    }, [hasLoadedEntityCounts, hasIngestedMetadata]);
-
+    console.log(recommendationModules)
     return (
         <RecommendationsContainer>
+           <Row gutter={32}>
             {orderedEntityCounts && orderedEntityCounts.length > 0 && (
-                <RecommendationContainer>
-                    <RecommendationTitle level={4}>Explore your Metadata</RecommendationTitle>
-                    <ThinDivider />
-                    {hasIngestedMetadata ? (
-                        <BrowseCardContainer>
+                <Col span={12}>
+                  <RecommendationContainer>
+                      <RecommendationTitle level={4}>元数据</RecommendationTitle>
+                      <ThinDivider />
+                      <BrowseCardContainer>
+
                             {orderedEntityCounts.map(
                                 (entityCount) =>
                                     entityCount &&
                                     entityCount.count !== 0 && (
+                                      
                                         <BrowseEntityCard
                                             key={entityCount.entityType}
                                             entityType={entityCount.entityType}
@@ -125,21 +95,15 @@ export const HomePageRecommendations = ({ userUrn }: Props) => {
                                         />
                                     ),
                             )}
-                        </BrowseCardContainer>
-                    ) : (
-                        <NoMetadataContainer>
-                            <NoMetadataEmpty description="No Metadata Found 😢" />
-                            <ConnectSourcesButton onClick={() => setShowGettingStartedModal(true)}>
-                                <RocketOutlined /> Connect your data sources
-                            </ConnectSourcesButton>
-                        </NoMetadataContainer>
-                    )}
-                </RecommendationContainer>
+                      </BrowseCardContainer>
+                  </RecommendationContainer>
+                </Col>
             )}
             {recommendationModules &&
-                recommendationModules.map((module) => (
+                [recommendationModules[0]].map((module) => (
+                  <Col span={12}>
                     <RecommendationContainer>
-                        <RecommendationTitle level={4}>{module.title}</RecommendationTitle>
+                        <RecommendationTitle level={4}>平台</RecommendationTitle>
                         <ThinDivider />
                         <RecommendationModule
                             key={module.moduleId}
@@ -148,8 +112,40 @@ export const HomePageRecommendations = ({ userUrn }: Props) => {
                             showTitle={false}
                         />
                     </RecommendationContainer>
+                  </Col>
                 ))}
-            <GettingStartedModal onClose={() => setShowGettingStartedModal(false)} visible={showGettingStartedModal} />
+            
+            {recommendationModules &&
+                [recommendationModules[1]].map((module) => (
+                  <Col span={12}>
+                    <RecommendationContainer>
+                        <RecommendationTitle level={4}>最近浏览</RecommendationTitle>
+                        <ThinDivider />
+                        <RecommendationModule
+                            key={module.moduleId}
+                            module={module as RecommendationModuleType}
+                            scenarioType={scenario}
+                            showTitle={false}
+                        />
+                    </RecommendationContainer>
+                  </Col>
+                ))}
+              {recommendationModules &&
+                [recommendationModules[2]].map((module) => (
+                  <Col span={12}>
+                    <RecommendationContainer>
+                        <RecommendationTitle level={4}>最受欢迎</RecommendationTitle>
+                        <ThinDivider />
+                        <RecommendationModule
+                            key={module.moduleId}
+                            module={module as RecommendationModuleType}
+                            scenarioType={scenario}
+                            showTitle={false}
+                        />
+                    </RecommendationContainer>
+                  </Col>
+                ))}
+          </Row>
         </RecommendationsContainer>
     );
 };
