eb90a058aa3ae28e81bff83319a115e75f6bde54
邵龙 <370440668@qq.com>
2022-11-28 19:58:33 +0800

    旧版本运行

--- datahub-web-react/src/setupProxy.js
+++ datahub-web-react/src/setupProxy.js
@@ -13,22 +13,25 @@ if (process.env.REACT_APP_MOCK === 'true' || process.env.REACT_APP_MOCK === 'cy'
         app.use(
             `${process.env.REACT_APP_PRODUCT==='true'?'':'/dev'}/logIn`,
             createProxyMiddleware(logInFilter, {
-                target: 'http://172.16.1.190:9002/',
+                target: 'http://192.168.3.114:9002/',
                 changeOrigin: true,
             }),
         );
         app.use(
             `${process.env.REACT_APP_PRODUCT==='true'?'':'/dev'}/authenticate`,
             createProxyMiddleware({
-                target: 'http://172.16.1.190:9002/',
+                target: 'http://192.168.3.114:9002/',
                 changeOrigin: true,
             }),
         );
         app.use(
             `${process.env.REACT_APP_PRODUCT==='true'?'':'/dev'}/api/v2/graphql`,
             createProxyMiddleware({
-                target: 'http://172.16.1.190:9002/',
+                target: 'http://192.168.3.114:9002/',
                 changeOrigin: true,
+                pathRewrite: {
+                  '^/dev': ''
+                }
             }),
         );
     };
